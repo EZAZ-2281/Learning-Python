0ac6c3a846f78ff6cf1c14677ba23d0d4fd49025
EZAZ-2281 <golamkibria3435@gmail.com>
2022-07-21 17:14:43 +0600

    py

---
+++ 3. Variables-and-Data-Types/main.py
@@ -0,0 +1,7 @@
+name = "Golam kibria"
+age = 22
+print("My name is: "+name); 
+print("My name is: "+name+ " Ezaz")
+print("Golam "+ "kibria "+ "Ezaz")
+print("I am ",age," years old")
+#string and int can not be Concatethats why we use comma. 
