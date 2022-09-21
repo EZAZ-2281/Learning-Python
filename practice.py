d1e319858bb965ab2edca334f95f67a079093a39
Ezaz <golamkibria3435@gmail.com>
2022-09-21 16:25:56 +0600

    js

--- practice.py
+++
@@ -1,4 +0,0 @@
-take_number = int(input())
-
-print("The next number for the number ",take_number," is",take_number+1,end=".\n")
-print("The previous number for the number ",take_number," is",take_number-1,end=".")
